dea83aacaed757f375c4d1e77a65ca8502a030af
HONGMUNIL <bear4564@naver.com>
2024-08-14 15:41:19 +0900

    research

---
+++ src/main/java/com/example/blog/domain/GeneratedValue.java
@@ -0,0 +1,5 @@
+package com.example.blog.domain;
+
+public @interface GeneratedValue {
+
+}
---
+++ src/main/java/com/example/blog/domain/GenerationType.java
@@ -0,0 +1,7 @@
+package com.example.blog.domain;
+
+public class GenerationType {
+
+    public static final String IDENTITY = null;
+
+}
